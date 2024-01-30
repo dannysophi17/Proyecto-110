1863b30cf35218305a97c64d67828afea2fa0ba9
dannysophi17 <122616673+dannysophi17@users.noreply.github.com>
2024-01-29 19:18:05 -0500

    Add files via upload

---
+++ Proyecto 110/tira un dado.py
@@ -0,0 +1,48 @@
+import random
+
+respuesta = "y"
+
+while respuesta == "y":
+    no = random.randint(1, 6)
+    
+    if no == 1:
+        print(" ------- ")
+        print("[       ]")
+        print("[   0   ]")
+        print("[       ]")
+        print(" ------- ")
+    elif no == 2:
+        print(" ------- ")
+        print("[ 0     ]")
+        print("[       ]")
+        print("[     0 ]")
+        print(" ------- ")
+    elif no == 3:
+        print(" ------- ")
+        print("[ 0     ]")
+        print("[   0   ]")
+        print("[     0 ]")
+        print(" ------- ")
+    elif no == 4:
+        print(" ------- ")
+        print("[ 0   0 ]")
+        print("[       ]")
+        print("[ 0   0 ]")
+        print(" ------- ")
+    elif no == 5:
+        print(" ------- ")
+        print("[ 0   0 ]")
+        print("[   0   ]")
+        print("[ 0   0 ]")
+        print(" ------- ")
+    elif no == 6:
+        print(" ------- ")
+        print("[ 0   0 ]")
+        print("[ 0   0 ]")
+        print("[ 0   0 ]")
+        print(" ------- ")
+    
+    respuesta = input("presiona y para rodar de nuevo, presiona n para salir: ")
+    
+    if respuesta != "y":
+        print("gracias por jugar")
